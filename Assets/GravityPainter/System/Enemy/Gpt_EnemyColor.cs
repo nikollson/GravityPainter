686c8ef6e07f668590ff9dbf96721cd60d36b81e
Hirukawa <hirujack@gmail.com>
2016-08-09 17:55:22 +0900

    エネミー基盤クラス途中コミット

---
+++ Assets/GravityPainter/System/Enemy/Gpt_EnemyColor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_EnemyColor : MonoBehaviour {
+
+    //Color 0=無色,1=赤,2=青,3=黄,4=紫,5=オレンジ,6=緑,
+    private int Color = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SetColor(int setColor)
+    {
+        Color = setColor;
+
+    }
+
+    int GetColor()
+    {
+        return Color;
+
+    }
+}
