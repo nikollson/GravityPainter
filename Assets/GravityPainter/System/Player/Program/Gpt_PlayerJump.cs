92f1141ce3b4badcfcc2fed031f5eaffb4928080
Sakata Yusuke <xxxaaaxxxcsharp@yahoo.co.jp>
2016-08-10 11:06:59 +0900

    Jumpボタンを押しっぱなしでジャンプが連続しないように修正

--- Assets/GravityPainter/System/Player/Program/Gpt_PlayerJump.cs
+++ Assets/GravityPainter/System/Player/Program/Gpt_PlayerJump.cs
@@ -11,16 +11,24 @@ public class Gpt_PlayerJump : MonoBehaviour {
     public float jumpTime = 0.2f;
     float jumpCount = 0;
 
-    public void StartJump()
+    int jumpPushFrame_log = 0;
+
+    public void StartJump(int JumpPushFrame)
     {
         jumpCount = 0;
         Vector3 speedDownXZPower = -1 * speedDownXZ * (rigidbody.velocity - new Vector3(0, rigidbody.velocity.y, 0));
         rigidbody.AddForce(jumpForce + speedDownXZPower, ForceMode.VelocityChange);
+        jumpPushFrame_log = JumpPushFrame;
     }
 
     public void EndJump()
     {
 
+    }
+
+    public bool CanStartJump(int jumpPushFrame)
+    {
+        return jumpPushFrame_log != jumpPushFrame;
     }
 
     public bool IsJumpEnd()
