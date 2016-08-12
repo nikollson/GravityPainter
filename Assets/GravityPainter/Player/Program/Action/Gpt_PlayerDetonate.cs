da7e73b7b935001e93a087566b96d66b139d88c7
Sakata Yusuke <xxxaaaxxxcsharp@yahoo.co.jp>
2016-08-12 14:31:09 +0900

    インク消費とインク回復の実装

--- Assets/GravityPainter/Player/Program/Action/Gpt_PlayerDetonate.cs
+++ Assets/GravityPainter/Player/Program/Action/Gpt_PlayerDetonate.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Gpt_PlayerDetonate : MonoBehaviour {
 
     public Gpt_PlayerUtillity playerUtillity;
+    public Gpt_PlayerInkManage playerInkManage;
+    public Gpt_PlayerState playerState;
     public float detonateEndTime = 0.5f;
     public float detonateDoTime = 0.3f;
     public float friction = 400;
@@ -47,6 +49,14 @@ public class Gpt_PlayerDetonate : MonoBehaviour {
     {
         detonateDone = true;
         playerUtillity.gravityManager.IsExplode();
+
+        float enemyPointSum = 0;
+        enemyPointSum += playerState.GetDetonateEnemyPoint(Gpt_InkColor.RED);
+        enemyPointSum += playerState.GetDetonateEnemyPoint(Gpt_InkColor.BLUE);
+        enemyPointSum += playerState.GetDetonateEnemyPoint(Gpt_InkColor.YELLOW);
+
+        playerInkManage.DoDetonateHeal(enemyPointSum);
+        playerState.Detonate();
     }
 
     public void UpdateDetonate()
---
+++ Assets/GravityPainter/Player/Program/Action/Gpt_PlayerInkManage.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_PlayerInkManage : MonoBehaviour
+{
+
+
+    public float inkMax = 1;
+
+    public float attack = 0.03f;
+    public float skill = 0.1f;
+    public float detonate = 0.1f;
+
+    public float autoHealPar = 0.5f;
+    public float autoHealSpeed = 0.2f;
+    
+    public float detonateHeal1 = 0.04f;
+    public float detonateHeal2 = 0.001f;
+
+
+    public float RestInk { get; private set; }
+
+    void Start()
+    {
+        MaxSet();
+    }
+
+    public void MaxSet() { RestInk = inkMax; }
+    public void AddInk(float value) { RestInk = Mathf.Min(inkMax, Mathf.Max(0, RestInk + value)); }
+    public void ConsumeInk(float value) { AddInk(-value); }
+
+    public bool CanUseAttack() { return RestInk >= attack; }
+    public bool CanUseSkill() { return RestInk >= skill; }
+    public bool CanUseDetonate() { return RestInk >= detonate; }
+
+    public void UseAttak() { ConsumeInk(attack); }
+    public void UseSkill() { ConsumeInk(skill); }
+    public void UseDetonate() { ConsumeInk(detonate); }
+
+    public void DoDetonateHeal(float enemyPoint)
+    {
+        float heal = detonateHeal1 * enemyPoint + detonateHeal2 * enemyPoint * enemyPoint;
+        AddInk(heal);
+    }
+
+    void Update()
+    {
+        if (RestInk < inkMax * autoHealPar)
+        {
+            RestInk = Mathf.Min(inkMax * autoHealPar, RestInk + Time.deltaTime * autoHealSpeed);
+        }
+    }
+
+}
