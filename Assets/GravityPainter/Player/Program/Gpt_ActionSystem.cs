6b33c0834cfcb1906642bb3c2134aae0459f91ef
Sakata Yusuke <xxxaaaxxxcsharp@yahoo.co.jp>
2016-08-12 12:30:40 +0900

    PlayerのMaterialがはがれていた問題を解決 スタートボタンでシーンをリロードできるように変更

--- Assets/GravityPainter/Player/Program/Gpt_ActionSystem.cs
+++ Assets/GravityPainter/Player/Program/Gpt_ActionSystem.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class Gpt_ActionSystem : MonoBehaviour {
-
-    public string sceneName;
-
+    
     void Update()
     {
         if (Gpt_Input.OptionDown)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
     }
 }
