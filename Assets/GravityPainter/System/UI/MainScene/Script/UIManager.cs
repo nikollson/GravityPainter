c075d74b8127aaf6df3919056f5096a8fe4dd02d
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-12 13:17:52 +0900

    UIとカメラ更新

--- Assets/GravityPainter/System/UI/MainScene/Script/UIManager.cs
+++ Assets/GravityPainter/System/UI/MainScene/Script/UIManager.cs
@@ -14,8 +14,11 @@ public class UIManager : MonoBehaviour {
     Vector3 notDrawPos = new Vector3(-10000,-10000,-10000);     // 非描画座標
 
     /* インクゲージ関連 */
-    public RawImage inkImg;
+    public RawImage inkImgBlue;
+    public RawImage inkImgYellow;
+    public RawImage inkImgRed;
     public RawImage inkBarImg;
+    Vector3 inkDrawPos;
 
     /* コンボ数関連 */
     public Text[] comboText = new Text[3];
@@ -25,6 +28,7 @@ public class UIManager : MonoBehaviour {
     void Start () {
         playerScript = player.GetComponent<Gpt_Player>();
         for (int i = 0; i < 12; i++) hpVecs[i] = hpImgs[i].transform.position;
+        inkDrawPos = inkImgBlue.transform.position;
     }
 
     // 更新関数
@@ -50,7 +54,18 @@ public class UIManager : MonoBehaviour {
     // インクゲージ更新関数
     void InkUpdate()
     {
-        inkImg.transform.localScale = new Vector3(playerScript.state.Ink / playerScript.state.inkMax, 1, 1);
+        // 全色を非描画領域に移動させる
+        inkImgBlue.transform.position = notDrawPos;
+        inkImgYellow.transform.position = notDrawPos;
+        inkImgRed.transform.position = notDrawPos;
+        // 色更新
+        if (playerScript.state.playerColor.Color == Gpt_InkColor.BLUE) inkImgBlue.transform.position = inkDrawPos;
+        else if (playerScript.state.playerColor.Color == Gpt_InkColor.YELLOW) inkImgYellow.transform.position = inkDrawPos;
+        else if (playerScript.state.playerColor.Color == Gpt_InkColor.RED) inkImgRed.transform.position = inkDrawPos;
+        // スケール更新
+        inkImgBlue.transform.localScale = new Vector3(playerScript.state.Ink / playerScript.state.inkMax, 1, 1);
+        inkImgYellow.transform.localScale = new Vector3(playerScript.state.Ink / playerScript.state.inkMax, 1, 1);
+        inkImgRed.transform.localScale = new Vector3(playerScript.state.Ink / playerScript.state.inkMax, 1, 1);
     }
 
     // コンボ数更新関数
