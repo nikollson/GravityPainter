56be0a751302f74c46cfdc46a064c9fb77905caa
松井浩昭 <ztskgo@gmail.com>
2016-08-17 02:28:13 +0900

    目の前に穴があったら止まる処理 暫定実装

--- Assets/Gpt_Enemy_AvoidHole.cs
+++ Assets/Gpt_Enemy_AvoidHole.cs
@@ -4,15 +4,26 @@ using System.Collections;
 public class Gpt_Enemy_AvoidHole : MonoBehaviour {
 
     Gpt_EnemyMove moveScript;
+    bool collFlg = true;        // 当たっているか
 
 	void Start () {
         moveScript = this.transform.parent.parent.GetComponent<Gpt_EnemyMove>();
     }
 
     void Update () {
-        //moveScript.SetSpeed(0.0f);
+        //moveScript.IsAbyssFalse();
+
+        // 当たっていない、つまり前に穴がある
+        if (!collFlg) {
+            moveScript.SetSpeed(0.0f);
+        }
+
+        collFlg = false;
     }
 
-    void OnTriggerEnter(Collider coll) {
+    void OnTriggerStay(Collider coll)
+    {
+        moveScript.SetSpeed(5.0f);
+        collFlg = true;
     }
 }
