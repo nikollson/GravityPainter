a2da367464554de68e03276f1caad8933fd80184
sakata yusuke <xxxaaaxxxcsharp@gmail.com>
2016-08-17 01:12:56 +0900

    タイルステージを生成する機能を追加

---
+++ Assets/GravityPainter/System/StageMake/Yuka/Gpt_YukaBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_YukaBox : MonoBehaviour {
+
+    public Renderer renderer;
+    
+    public void SetColor(Material material)
+    {
+        renderer.sharedMaterial = material;
+    }
+
+}
