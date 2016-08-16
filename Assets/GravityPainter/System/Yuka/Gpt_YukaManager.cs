399ad3266bfc000bc570c0440e5c35b1fcb7b01b
Sakata Yusuke <xxxaaaxxxcsharp@yahoo.co.jp>
2016-08-16 19:01:31 +0900

    床実装途中(マージしなくてもOK）

---
+++ Assets/GravityPainter/System/Yuka/Gpt_YukaManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CanEditMultipleObjects]
+//[CustomEditor(typeof(CustomSprite))]
+public class Gpt_YukaManager : MonoBehaviour {
+
+    public Transform allTileParent;
+
+    public Sprite stageTile;
+    public float tileSize = 3;
+
+    public ReadTileSetting readTileSetting;
+
+    void Start() { if (Application.isPlaying) Start_Normal(); }
+    void Update()
+    {
+        if (!Application.isPlaying) Update_Editor();
+        if (Application.isPlaying) Update_Normal();
+    }
+
+    void Start_Normal()
+    {
+
+    }
+
+
+
+    void Update_Normal()
+    {
+
+    }
+
+
+
+
+
+    void Update_Editor()
+    {
+        if (allTileParent.localScale.x != tileSize) allTileParent.localScale = new Vector3(tileSize, tileSize, tileSize);
+    }
+
+    [System.Serializable]
+    public class ReadTileSetting
+    {
+        public Color white;
+        public Color hole;
+        public Color red;
+        public Color blue;
+        public Color yellow;
+        public Color redRandom;
+        public Color blueRandom;
+        public Color yellowRandom;
+    }
+}
