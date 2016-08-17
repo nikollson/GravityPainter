d0722d87cdeed8097730ff3dbb7c26cc7d64e75b
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-17 11:29:15 +0900

    床を避けるシステムのバグを修正

--- Assets/GravityPainter/System/Enemy/Gpt_Enemy_AvoidHole.cs
+++ Assets/GravityPainter/System/Enemy/Gpt_Enemy_AvoidHole.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gpt_Enemy_AvoidHole : MonoBehaviour {
+public class Gpt_Enemy_AvoidHole : MonoBehaviour
+{
 
     Gpt_EnemyMove moveScript;
+    public HitManager hitManager;
     bool collFlg = true;        // 当たっているか
+    float cnt = 0.0f;
 
-	void Start () {
+    void Start()
+    {
         moveScript = this.transform.parent.parent.GetComponent<Gpt_EnemyMove>();
     }
 
-    void Update () {
+    void Update()
+    {
 
-        if (!collFlg)
+        if (!hitManager.IsHit)
         {
             // 当たっていない、つまり前に穴がある際の処理
             moveScript.IsAbyss();
+            Debug.Log("!!!!!!!!!!!!");
         }
-        else {
+        else
+        {
             // 当たっている、つまり前に穴がない際の処理
             moveScript.IsAbyssFalse();
+            Debug.Log("あたっている状態");
         }
+        cnt = 0.0f;
 
-        collFlg = false;
+        //collFlg = false;
     }
 
+    /*
     // Update関数より先に呼ばれる
     void OnTriggerStay(Collider coll)
     {
         collFlg = true;
     }
+    */
 }
