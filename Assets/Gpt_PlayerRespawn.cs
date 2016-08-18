b0a58237b13facd75f2e96fe3fa6cd6ba7bc0489
sakata yusuke <xxxaaaxxxcsharp@gmail.com>
2016-08-19 02:57:09 +0900

    復活床の追加

---
+++ Assets/Gpt_PlayerRespawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_PlayerRespawn : MonoBehaviour {
+
+    public GameObject respawnFloorPrefab;
+    public Transform respawnFloorPosition;
+    
+    public void DoRespawn()
+    {
+        MakeFloor();
+    }
+
+    public void MakeFloor()
+    {
+        Instantiate(respawnFloorPrefab, respawnFloorPosition.position, Quaternion.identity);
+    }
+}
