131e49e88658eab63424f71516b7893bb3fb5461
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-18 17:39:42 +0900

    ボス戦闘大幅更新、えふぇくと大量投入(すいません重いです)

---
+++ Assets/GravityPainter/GolemButtle/Models/Golem/Script/Gpt_BossAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_BossAI : MonoBehaviour
+{
+    // 行動パターン
+    enum State
+    {
+        None = 0,           // 完全に何もしない
+        Search = 1,         // 探し(周ってプレイヤーを探している、リスポーン地点にいる際に使用する)
+        StrikeAtk = 10,     // 攻撃
+    }
+    State state;
+
+    float cnt = 0.0f;
+
+    void Start()
+    {
+        state = State.Search;
+    }
+
+    void Update()
+    {
+
+        cnt += Time.deltaTime;
+
+        // 探し状態
+        if (state == State.Search)
+        {
+            this.transform.eulerAngles = new Vector3(0, cnt*50.0f, 0);
+        }
+    }
+}
