ca0c358269d48503436efb69d3280b41f639eb5d
Hirukawa <hirujack@gmail.com>
2016-08-18 14:44:28 +0900

    中身は河村　エフェクト追加

---
+++ Assets/Gpt_EnemyNavigator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_EnemyNavigator : MonoBehaviour {
+
+
+    public Transform target;
+    public NavMeshAgent navigator;
+	// Use this for initialization
+	void Start () {
+        navigator.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
