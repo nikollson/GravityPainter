06f881f42276f24ddfbc8858fbc3bee5fa074135
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-19 11:47:12 +0900

    プレイヤー落下ダメ実装など

---
+++ Assets/GravityPainter/GolemButtle/Script/Gpt_BossBattleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_BossBattleManager : MonoBehaviour {
+
+    public GameObject player;
+    public float fallY = -20.0f;
+    public Vector3 playerResPos;
+
+    void Start () {
+	}
+	
+	void Update () {
+
+        if (player.transform.position.y < fallY)
+        {
+            player.GetComponent<Gpt_PlayerState>().AddHPDamage(4);
+            player.transform.position = playerResPos;
+        }
+    }
+}
