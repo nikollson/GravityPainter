2a8e932842cd5f222ff548c90d9395ff49016203
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-19 13:54:29 +0900

    ボスが死ぬときの実装以外の超暫定ボス完成Ver

--- Assets/Gpt_ToPlayerColl.cs
+++ Assets/Gpt_ToPlayerColl.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class Gpt_ToPlayerColl : MonoBehaviour {
 
     public GameObject player;
+    public GameObject boss;
 
-	void Start () {
+    void Start () {
 	}
 	
 	void Update () {
@@ -13,6 +14,15 @@ public class Gpt_ToPlayerColl : MonoBehaviour {
 
     void OnTriggerEnter(Collider c)
     {
-        if(c.tag=="Player") player.GetComponent<Gpt_PlayerState>().AddHPDamage(3);
+        if (c.tag == "Player")
+        {
+            player.GetComponent<Gpt_PlayerState>().AddHPDamage(1);
+
+        }
+        //else if (c.tag != "Enemy")
+        else if (c.tag != "Enemy")
+        {
+            boss.GetComponent<Gpt_Boss>().state = 1;
+        }
     }
 }
--- Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
+++ Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
@@ -5,11 +5,27 @@ public class Gpt_Boss : MonoBehaviour {
 
     const float maxHp = 10.0f;
     float hp = 10.0f;
+    public GameObject hand1;
+    public GameObject hand2;
+    public int state = 1;
 
     void Start () {
 	}
 	
 	void Update () {
+
+        if (state==0)
+        {
+            // 通常状態
+        }
+        else if (state == 1)
+        {
+            this.transform.position += new Vector3(0, Time.deltaTime * 100.0f, 0);
+        }
+
+        this.transform.position += new Vector3(0, Time.deltaTime * 100.0f, 0);
+
+        Debug.Log("BOSS_STATE: "+state);
 	}
 
     public float GetMaxHp()
@@ -21,4 +37,20 @@ public class Gpt_Boss : MonoBehaviour {
     {
         return hp;
     }
+
+    public void AddHP(int plus)
+    {
+        hp += plus;
+    }
+
+    //void OnTriggerEnter(Collider c)
+    //{
+    //    if (c.tag != "Player" && c.tag != "Enemy")
+    //    {
+    //        Debug.Log("AA");
+    //    }
+
+    //    Debug.Log("AAaaa");
+
+    //}
 }
