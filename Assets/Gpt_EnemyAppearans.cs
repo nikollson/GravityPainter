4e4ea1662a7c0e4d70a51e73278d60bf0006799f
Sakata Yusuke <xxxaaaxxxcsharp@yahoo.co.jp>
2016-08-19 18:39:41 +0900

    敵の出現を少し修正

---
+++ Assets/Gpt_EnemyAppearans.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_EnemyAppearans : MonoBehaviour
+{
+    private Gpt_YukaManager yukaManager;
+
+    void Start()
+    {
+        yukaManager = GameObject.Find("YukaManager").GetComponent<Gpt_YukaManager>();
+
+        foreach (Transform a in this.transform)
+        {
+            Debug.Log(this.gameObject.name + " " + yukaManager.GetTileCordinate(a.transform.position));
+            if (!yukaManager.HasTile(a.position))
+            {
+                a.gameObject.SetActive(false);
+            }
+        }
+    }
+}
