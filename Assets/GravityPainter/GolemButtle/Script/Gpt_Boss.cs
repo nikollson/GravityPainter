c9c8331c62f9e57214d419d608748ae501b9da20
Hiroaki Matsui <ztskgo@gmail.com>
2016-08-19 20:13:46 +0900

    ステートをさらに追加

--- Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
+++ Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
@@ -8,6 +8,7 @@ public class Gpt_Boss : MonoBehaviour {
         Wait = 0,
         Search,
         Fall,
+        Up,
         Atk1,
         Atk2,
         Atk3, 
@@ -16,10 +17,17 @@ public class Gpt_Boss : MonoBehaviour {
 
     const float maxHp = 10.0f;
     float hp = 10.0f;
+
     public GameObject hand1;
     public GameObject hand2;
-    float fallSpd = 10.0f;
+
+    float fallSpd = 10.0f;      // 落下速度
+    float upSpd = 25.0f;        // 上昇速度
+    float fallY = -15.0f;       // マグマY位置()
+    float magmaDmg = 2.5f;
     float cnt = 0.0f;
+    public Vector3 firstBossPos;
+
 
     void Start () {
 	}
@@ -28,21 +36,63 @@ public class Gpt_Boss : MonoBehaviour {
 
         cnt += Time.deltaTime;
 
+        // 最初の行動パターン
         if (state == State.Wait)
         {
-            //
-        }
-        else if (state == State.Fall)
-        {
-            this.transform.position += new Vector3(0, -Time.deltaTime* fallSpd, 0);
-            if(this.transform.position.y < -15.0f)
+            // 乱数生成
+            float rnd = Random.Range(0.0f, 1.0f);
+            // 行動をランダムに決定する
+            if (rnd < 0.33f)
             {
-                state = 0;
-                this.hp -= 2.5f;
-                this.transform.position = new Vector3(0,0.23f,0);
+                state = State.Atk1;
+            }
+            else if(rnd < 0.66f)
+            {
+                state = State.Atk2;
+            }
+            else
+            {
+                state = State.Atk3;
             }
         }
-	}
+        // 落下ステート
+        else if (state == State.Fall)
+        {
+            // 落下ベクトルを足す
+            this.transform.position += new Vector3(0, -Time.deltaTime * fallSpd, 0);
+            // 一定まで落ちると被ダメ
+            if(this.transform.position.y < fallY)
+            {
+                state = State.Up;
+                this.hp -= magmaDmg;
+            }
+        }
+        // 上昇ステート(簡易制御)
+        else if (state == State.Up)
+        {
+            // 上昇ベクトルを足す
+            this.transform.position += new Vector3(0, Time.deltaTime * upSpd, 0);
+
+            // 元々いた位置まで上昇すれば
+            if (this.transform.position.y < firstBossPos.y)
+            {
+                this.transform.position = new Vector3(0, firstBossPos.y, 0);
+                state = State.Wait;
+            }
+        }
+        else if (state == State.Atk1)
+        {
+
+        }
+        else if (state == State.Atk2)
+        {
+
+        }
+        else if (state == State.Atk3)
+        {
+
+        }
+    }
 
     public float GetMaxHp()
     {
