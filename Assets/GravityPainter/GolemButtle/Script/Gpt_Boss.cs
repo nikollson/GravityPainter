903a9e8f239acd8acddf49f3817a5ec9889a1a3f
松井浩昭 <ztskgo@gmail.com>
2016-08-22 00:32:55 +0900

    薙ぎ払い攻撃

--- Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
+++ Assets/GravityPainter/GolemButtle/Script/Gpt_Boss.cs
@@ -80,7 +80,7 @@ public class Gpt_Boss : MonoBehaviour
         else if (state == State.SelectAtk)
         {
             float rnd = UnityEngine.Random.Range(0.0f, 100.0f);
-            if (hp >= 7.0f)
+            if (hp >= 70.0f)
             {
                 if (rnd < 50.0f)
                 {
@@ -97,19 +97,19 @@ public class Gpt_Boss : MonoBehaviour
             }
             else
             {
-                if (rnd < 33.33f)
-                {
-                    state = State.Atk1;
-                    attackTime = 0.0f;
-                    anim.SetBool("Atk_L_Flg", true);
-                }
-                else if (rnd < 66.66f)
-                {
-                    state = State.Atk2;
-                    attackTime = 0.0f;
-                    anim.SetBool("Atk_R_Flg", true);
-                }
-                else
+                //if (rnd < 33.33f)
+                //{
+                //    state = State.Atk1;
+                //    attackTime = 0.0f;
+                //    anim.SetBool("Atk_L_Flg", true);
+                //}
+                //else if (rnd < 66.66f)
+                //{
+                //    state = State.Atk2;
+                //    attackTime = 0.0f;
+                //    anim.SetBool("Atk_R_Flg", true);
+                //}
+                //else
                 {
                     state = State.Atk3;
                     attackTime = 0.0f;
@@ -192,10 +192,33 @@ public class Gpt_Boss : MonoBehaviour
         else if (state == State.Atk3)
         {
             attackTime += Time.deltaTime;
-            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Select") && attackTime >= 0.5f)
+
+            // 途中で床は壊れる
+            // プレイヤーが下にいる
+            if (player.transform.position.y < 12.0f)
+            {
+                if (attackTime >= 2.0f && attackTime <= 2.99f)
+                {
+                    yuka[(targetYukaNum +1) % 8].GetComponent<Gpt_YukaBox>().AddDamage(1);
+                    attackTime += 1.0f;
+                }
+                else if (attackTime >= 4.5f && attackTime <= 5.49f)
+                {
+                    yuka[(targetYukaNum) % 8].GetComponent<Gpt_YukaBox>().AddDamage(1);
+                    attackTime += 1.0f;
+                }
+                else if (attackTime >= 7.0f && attackTime <= 7.99f)
+                {
+                    yuka[(targetYukaNum - 1) % 8].GetComponent<Gpt_YukaBox>().AddDamage(1);
+                    attackTime += 1.0f;
+                }
+            }
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Select") && attackTime >= ATTACK_TIME_MAX)
             {
                 anim.SetBool("Atk_Nagi_Flg", false);
                 state = State.Search;
+                attackTime = 0.0f;
             }
         }
     }
