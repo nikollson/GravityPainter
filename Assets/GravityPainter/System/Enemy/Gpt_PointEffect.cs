3be6547e04c9a004db8babadbeb54a72f6d7617a
Hirukawa <hirujack@gmail.com>
2016-08-22 10:12:27 +0900

    一匹目の敵に攻撃時に爆発範囲のポイント追加

---
+++ Assets/GravityPainter/System/Enemy/Gpt_PointEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gpt_PointEffect : MonoBehaviour {
+    public ParticleSystem ps;
+
+    //止まる時間
+    public float stopTime = 4f;
+    private float stop;
+    // Use this for initialization
+    void Start()
+    {
+        
+
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+        stop += 0.1f;
+        if (stop > stopTime)
+        {
+            ps.Pause();
+        }
+    }
+}
